be023b200d4bca8cf507f950aca532398cc22054
James Schultz <ninjames101@gmail.com>
2018-03-12 12:45:14 -0600

    initial commit, still need to fix readme. First half of js file complete

---
+++ index.js
@@ -0,0 +1,94 @@
+// 1. declare a variable called person
+//    set the person variable equal to an object literal
+//      with a property first_name with a string value of your first name
+//      with property last_name with a string value of your last name
+var person = {
+    first_name: "James",
+    last_name: "Schultz"
+}
+
+// 2. declare a variable called cart
+//    set the cart variable equal to an object literal
+//      with a property items with an array value of ['hat', 'shirt', 'shoes']
+var cart = {
+    items: ['hat','shirt','shoes']
+
+}
+
+// 3. declare a variable called zoo
+//    set the zoo variable equal to an object literal
+//      with a property animals with an array value of ['lion', 'tiger', 'penguin']
+//      with a property location with a string value of 'Denver, CO'
+//      with a property price with a number value of 9.99
+var zoo = {
+    animals: ['lion', 'tiger', 'penguin'],
+    location: "Denver, CO",
+    price: 9.99
+}
+
+
+// 4. declare a variable called galvanize
+//    set the galvanize variable equal to an object literal
+//     with a property cohorts with an array value of ['g64', 'g70', 'g75']
+//     with a property instructors with an array value of ['CJ', 'Casey', 'Kim', 'Kyle', 'Dan', 'Marlena', 'Winzer', 'Brooks', 'James']
+//     with a property students with an array value including yourself and 5 other students names
+var galvanize = {
+  cohorts: ['g64', 'g70', 'g75'],
+  instructors: ['CJ', 'Casey', 'Kim', 'Kyle', 'Dan', 'Marlena', 'Winzer', 'Brooks', 'James'],
+  students: ['james','peter','CJ','kim','cass', 'casey']
+}
+
+// 5. declare a variable called standard
+//    set the standard variable equal to an object literal
+//      with a property name with a string value of 'Use basic programming skills to solve problems'
+//      with a property domain with a string value of "Demonstrate proficiency in a programming language"
+//      with a property code with a string value of 'TS-2'
+var standard = {
+    name: "Use basic programming skills to solve problems",
+    domain: "Demonstrate proficiency in a programming language",
+    code: "TS-2"
+}
+
+
+
+
+
+// 1a) Log the first_name property of the person object using dot notation
+
+
+// 2a) Log the 0th index of the items property of the cart object using dot notation
+
+
+
+// 3a) Log the tiger from the items property of the zoo object using dot notation
+
+
+
+
+// 4a). Log the value of the instructors property from the galvanize object using dot notation
+//    use the property variable
+
+const property = 'instructors';
+
+
+// 6. What will log to the console?
+
+let posts = [
+  {
+    time: '1259781232',
+    type: 12,
+    message: 'Cowabunga!!',
+    tags: ['pizza', 'turtle power']
+  },
+  {
+    time: null,
+    type: 0,
+    message: null,
+    tags: []
+  }
+];
+
+// console.log(posts[0].tags[1]); // What will log to the console?
+
+
+
